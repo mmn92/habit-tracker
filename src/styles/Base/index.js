8125e04a4bc5596d64b53e53fb1e3b9da36c37dc
mmn92 <matheus.mota.nogueira@gmail.com>
2019-04-14 23:14:59 -0300

    add Base style file

---
+++ src/styles/Base/index.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Base = createGlobalStyle`
+    body {
+        font-family: 'Quicksand', sans-serif;
+    }
+    button {
+        font-family: 'Quicksand', sans-serif;
+        height: 35px;
+        width: 80px;
+    }
+`;
+
+export default Base;
