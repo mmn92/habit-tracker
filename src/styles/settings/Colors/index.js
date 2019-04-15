efd79d3de29964cfbc86c1fef76c28df20df8d0f
mmn92 <matheus.mota.nogueira@gmail.com>
2019-04-14 23:53:45 -0300

    add base colors to project

---
+++ src/styles/settings/Colors/index.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Colors = createGlobalStyle`
+    :root {
+        --color-negative: #393e46;
+        --color-negative-dark: #222831;
+        --color-positive: #EEEEEE;
+        --color-positive-dark: #818D92;
+        --color-zero: #FFFFFF;
+        --color-green-progress: #AAFFAA;
+    }
+`;
+
+export default Colors;
