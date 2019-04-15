c2c4baa7b689604801bf0588a7cd5a3f2ff3245c
mmn92 <matheus.mota.nogueira@gmail.com>
2019-04-14 22:48:39 -0300

    add general style file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Reset from './styles/generic/Reset';
+
+ReactDOM.render(<Fragment>
+                    <Reset />
+                    <App />
+                </Fragment>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
